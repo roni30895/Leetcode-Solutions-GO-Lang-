bdba420fa313a9e919e280f4aee7cc02d65c286e
Rohan Nanabhau Pawar <106943166+roni30895@users.noreply.github.com>
2023-02-20 12:11:30 +0530

    Create 136 Single number.go

---
+++ 136 Single number.go
@@ -0,0 +1,11 @@
+func singleNumber(nums []int) int {
+    
+	if len(nums) == 1 {
+		return nums[0]
+	} 
+    result:=nums[0]
+    for i:=1; i< len(nums); i++ {
+        result=result^nums[i]
+    }
+    return result
+}
