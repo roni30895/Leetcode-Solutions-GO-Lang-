efbefcd5a10e8a584a7f9959c700618cf03ca4e8
Rohan Nanabhau Pawar <106943166+roni30895@users.noreply.github.com>
2023-02-20 12:12:04 +0530

    Create 144. Binary Tree Preorder Traversal.go

---
+++ 144. Binary Tree Preorder Traversal.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+    var ans []int
+    traverse(root, &ans)
+    return ans
+}
+
+func traverse(root *TreeNode, ans *[]int) {
+    if root == nil {
+        return
+    }
+    
+    *ans = append(*ans, root.Val)
+    traverse(root.Left, ans)
+    traverse(root.Right, ans)
+    
+}
