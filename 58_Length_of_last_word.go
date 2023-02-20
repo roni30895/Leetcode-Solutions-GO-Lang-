94e33376569518c904d273becb9238b24853e1c4
Rohan Nanabhau Pawar <106943166+roni30895@users.noreply.github.com>
2023-02-20 09:31:26 +0530

    Update 58_Length_of_last_word.go

--- 58_Length_of_last_word.go
+++ 58_Length_of_last_word.go
@@ -17,3 +17,12 @@ func lengthOfLastWord(s string) int {
 	return 0
  
 }
+
+// OR //
+
+func lengthOfLastWord(s string) int {
+    str := strings.TrimSpace(s)
+    words := strings.Split(str," ")
+    last_word := words[len(words)-1]
+    return len(last_word)
+}
